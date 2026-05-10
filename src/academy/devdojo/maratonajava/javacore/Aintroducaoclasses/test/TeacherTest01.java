a89c9c31934c01ca70387d4d28fbffbd9c020f11
Nattan <natanaelsouzza123@gmail.com>
2026-05-10 17:06:00 -0300

    coesão

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/TeacherTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Teacher;
+
+public class TeacherTest01 {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.name = "Vanildo";
+        teacher.age = 89;
+        teacher.gender = 'M';
+    }
+}
