f8eac25d54867e90c213b1ea8ebb08589ed072f5
Nattan <natanaelsouzza123@gmail.com>
2026-05-10 17:28:42 -0300

    car

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Car;
+
+public class CarTest01 {
+    Car car01 = new Car();
+    Car car02 = new Car();
+
+    car01.name = "Civic";
+
+    System.out.println(car01.name);
+}
