7ad46de62a97b263d5980d6ad7639cc685eb8202
Nattan <natanaelsouzza123@gmail.com>
2026-05-16 12:05:47 -0300

    Update CarTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarTest01.java
@@ -3,10 +3,20 @@ package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
 import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Car;
 
 public class CarTest01 {
-    Car car01 = new Car();
-    Car car02 = new Car();
+    public static void main(String[] args){
+        Car car01 = new Car();
+        Car car02 = new Car();
 
-    car01.name = "Civic";
+        car01.name = "Civic";
+        car01.model = "4x4";
+        car01.year = 2024;
 
-    System.out.println(car01.name);
+        car02.name = "Volkswagen";
+        car02.model = "1.4 Turbo";
+        car02.year = 2018;
+
+        System.out.println("Nome: " + car01.name + ", Modelo: " + car01.model + ", Ano: " + car01.year);
+        System.out.println("Nome: " + car02.name + ", Modelo: " + car02.model + ", Ano: " + car02.year);
+
+    }
 }
