83c4db4bc260340020b041058bb8cf1bbe4b771f
Nattan <natanaelsouzza123@gmail.com>
2026-05-23 11:12:39 -0300

    introdução metodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculator.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculator {
+    public void sumTwoNumber() {
+        System.out.println(10 + 10);
+    }
+}
