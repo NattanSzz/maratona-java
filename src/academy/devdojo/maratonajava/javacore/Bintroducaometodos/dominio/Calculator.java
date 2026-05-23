76c36ac816f1dbe8eec07d8e9ac5eb71e57f06b9
Nattan <natanaelsouzza123@gmail.com>
2026-05-23 13:01:38 -0300

    Update Calculator.java

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculator.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculator.java
@@ -22,6 +22,9 @@ public class Calculator {
     }
 
     public double divideTwoNumber(double num1, double num2) {
+        if(num2 == 0) {
+            return 0;
+        }
         return num1 / num2;
     }
 }
