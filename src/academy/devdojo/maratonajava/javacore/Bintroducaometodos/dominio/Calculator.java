2d9215d1a51cf1bc6a860d6ece9ba64cdd7e9e1e
Nattan <natanaelsouzza123@gmail.com>
2026-05-23 11:37:25 -0300

    calculator

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculator.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculator.java
@@ -4,4 +4,20 @@ public class Calculator {
     public void sumTwoNumber() {
         System.out.println(10 + 10);
     }
+
+    public void subtractTwoNumbers() {
+        System.out.println(1 - 2);
+    }
+
+    public void multiplyTwoNumber(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public void sumArrayValues(int[] arr) {
+        int result = 0;
+        for (int v : arr) {
+            result += v;
+        }
+        System.out.println(result);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculatorTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculatorTest02.java
@@ -2,11 +2,12 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculator;
 
-public class CalculatorTest01 {
+public class CalculatorTest02 {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
-        calculator.sumTwoNumber();
-        calculator.subtractTwoNumbers();
+        int[] values = {1, 2, 3, 4, 7, 15};
+        
+        calculator.multiplyTwoNumber(10, 2);
+        calculator.sumArrayValues(values);
     }
 }
