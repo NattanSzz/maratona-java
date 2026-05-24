a8b5d4f87ef6268a84cb15d4dacc5ab7cd5019de
Nattan <natanaelsouzza123@gmail.com>
2026-05-24 17:36:04 -0300

    blocos de inicialização

---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/domain/Movie.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain;
+
+public class Movie {
+    private String name;
+    private int[] episodes;
+
+    // Alocado espaço em memoria para o objeto
+    // Atributos inicializados por default ou o que for passado
+    // Bloco de inicialização é executado
+    // Construtor é executado
+
+    {
+        episodes = new int[100];
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = i + 1;
+        }
+    }
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public Movie() {
+        
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/test/MovieTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain.Movie;
+
+public class MovieTest01 {
+    public static void main(String[] args) {
+        Movie movie = new Movie("One Piece");
+
+        System.out.println(movie.getEpisodes());
+        for (int ep : movie.getEpisodes()) {
+            System.out.print(ep + " ");
+        }
+    }
+}
