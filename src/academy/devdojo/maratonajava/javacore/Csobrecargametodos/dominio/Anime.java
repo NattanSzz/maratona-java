d286811c5f1f809caf758e60269bf324024bc76c
Nattan <natanaelsouzza123@gmail.com>
2026-05-24 12:01:48 -0300

    sobrecarga de metodos

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,58 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String name;
+    private String type;
+    private int episodes;
+    private String gender;
+
+    public void init(String name, String type, int episodes) {
+        this.name = name;
+        this.type = type;
+        this.episodes = episodes;
+    }
+
+    public void init(String name, String type, int episodes, String gender) {
+        this.init(name, type, episodes);
+        this.gender = gender;
+    }
+
+    public void print() {
+        System.out.println(this.name);
+        System.out.println(this.type);
+        System.out.println(this.episodes);
+        System.out.println(this.gender);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getEpisodes() {
+        return this.episodes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+}
