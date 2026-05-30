98d52e0cc01dcbc5c9eeabf085ff8aaf80a049e7
Nattan <natanaelsouzza123@gmail.com>
2026-05-30 13:34:01 -0300

    static

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Car.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Car.java
@@ -3,7 +3,41 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
 public class Car {
     private String name;
     private double maxVelocity;
-    private double limitVelocity = 250;
+    public static double limitVelocity = 250;
 
-    
+    public Car(String name, double maxVelocity) {
+        this.name = name;
+        this.maxVelocity = maxVelocity;
+    }
+
+    public void print() {
+        System.out.println("----------------");
+        System.out.println("Nome: " + this.name);
+        System.out.println("Velocidade Maxima: " + this.maxVelocity);
+        System.out.println("Limite de velocidade: " + Car.limitVelocity);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxVelocity() {
+        return this.maxVelocity;
+    }
+
+    public void setMaxVelocity(double maxVelocity) {
+        this.maxVelocity = maxVelocity;
+    }
+
+    public double getLimitVelocity() {
+        return limitVelocity;
+    }
+
+    public void setLimitVelocity(double limitVelocity) {
+        this.limitVelocity = limitVelocity;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Car;;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car car01 = new Car("BMW", 280);
+        Car car02 = new Car("Mercedes", 275);
+        Car car03 = new Car("Audi", 290);
+
+        Car.limitVelocity = 180;
+
+        car01.print();
+        car02.print();
+        car03.print();
+    }
+}
