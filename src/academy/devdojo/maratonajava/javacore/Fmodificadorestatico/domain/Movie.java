500200e8b966242e9c686b969614d963291d8287
Nattan <natanaelsouzza123@gmail.com>
2026-05-30 14:29:28 -0300

    blocos de inicialização estaticos

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Movie.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Movie {
+    private String name;
+    private static int[] episodes;
+
+    // Bloco de inicialização é carregado quando a JVM carregar a classe
+    // Alocado espaço em memoria para o objeto
+    // Atributos inicializados por default ou o que for passado
+    // Bloco de inicialização é executado
+    // Construtor é executado
+
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodes = new int[100];
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = i + 1;
+        }
+    }
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public Movie() {
+        for(int ep : Movie.episodes) {
+            System.out.print(ep + " ");
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/MovieTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+//import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Movie;
+
+public class MovieTest01 {
+    public static void main(String[] args) {
+        //Movie movie1 = new Movie();
+        //Movie movie2 = new Movie();
+        //Movie movie3 = new Movie();
+    }
+}
