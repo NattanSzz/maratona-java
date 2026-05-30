e8144b42264b6a4314a11640972da6be026ca542
Nattan <natanaelsouzza123@gmail.com>
2026-05-30 12:37:50 -0300

    Create Car.java

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Car.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Car {
+    private String name;
+    private double maxVelocity;
+    private double limitVelocity = 250;
+
+    
+}
