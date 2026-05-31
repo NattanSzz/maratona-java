8a60a8e7cfb81eb9a10118b2c8ce53113f6d409a
Nattan <natanaelsouzza123@gmail.com>
2026-05-31 14:18:14 -0300

    modificador final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Car.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Car {
+    private String name;
+    public static final double LIMIT_VELOCITY = 250;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        // Car car = new Car();
+        System.out.println(Car.LIMIT_VELOCITY);
+    }
+}
