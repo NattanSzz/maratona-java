7bc2384111a545252e19bf62ece09116c475487b
Nattan <natanaelsouzza123@gmail.com>
2026-05-31 14:40:41 -0300

    final para metodos e classes

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Car.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Car.java
@@ -5,6 +5,10 @@ public class Car {
     public static final double LIMIT_VELOCITY = 250;
     public final Buyer BUYER = new Buyer();
 
+    public final void print() {
+        System.out.println(this.name);
+    }
+
     public String getName() {
         return name;
     }
--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Car;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class CarTest01 {
     public static void main(String[] args) {
@@ -8,5 +9,9 @@ public class CarTest01 {
         System.out.println(Car.LIMIT_VELOCITY);
         car.BUYER.setName("Toreto");
         System.out.println(car.BUYER);
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.setName("Furioso");
+        ferrari.print();
     }
 }
