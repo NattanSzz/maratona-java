5a434796495130cb024a021b3d7fd3ae4a676f21
Nattan <natanaelsouzza123@gmail.com>
2026-06-04 11:51:31 -0300

    interfaces

---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public class DatabaseLoader implements DataLoader {
+    public void load() {
+        System.out.println("Carregando dados...");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/FileLoader.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public class FileLoader implements DataLoader {
+    public void load() {
+        System.out.println("Carregando dados de um arquivo...");
+    }
+}
