8fddb9eca6866903641298740ff6f26a2f3ab858
Nattan <natanaelsouzza123@gmail.com>
2026-06-16 10:48:49 -0300

    string performance

--- src/academy/devdojo/maratonajava/javacore/Qstrings/test/StringPerformanceTest.java
+++ src/academy/devdojo/maratonajava/javacore/Qstrings/test/StringPerformanceTest.java
@@ -1,5 +1,41 @@
 package academy.devdojo.maratonajava.javacore.Qstrings.test;
 
 public class StringPerformanceTest {
-    
+    public static void main(String[] args) {
+        long init = System.currentTimeMillis();
+        concatString(10_000);
+        long end = System.currentTimeMillis();
+        System.out.println("Tempo: " + (end - init) + "ms");
+
+        init = System.currentTimeMillis();
+        concatString(100_000);
+        end = System.currentTimeMillis();
+        System.out.println("Tempo: " + (end - init) + "ms");
+
+        init = System.currentTimeMillis();
+        concatString(100_000);
+        end = System.currentTimeMillis();
+        System.out.println("Tempo: " + (end - init) + "ms");
+    }
+
+    public static void concatString(int lenght) {
+        String text = "";
+        for(int i = 0; i < lenght; i++){
+            text += i;
+        }
+    }
+
+    public static void concatStringBuilder(int lenght) {
+        StringBuilder text = new StringBuilder(lenght);
+        for(int i = 0; i < lenght; i++){
+            text.append(i);
+        }
+    }
+
+    public static void concatStringBuffer(int lenght) {
+        StringBuffer text = new StringBuffer(lenght);
+        for(int i = 0; i < lenght; i++){
+            text.append(i);
+        }
+    }
 }
