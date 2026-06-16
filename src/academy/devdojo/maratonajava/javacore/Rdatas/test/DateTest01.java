1f295f91e8aaf3e266ee56e8dc294a8e29088f0f
Nattan <natanaelsouzza123@gmail.com>
2026-06-16 12:24:26 -0300

    Create DateTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(17_816_231_642_24L);
+        System.out.println(date);
+    }
+}
