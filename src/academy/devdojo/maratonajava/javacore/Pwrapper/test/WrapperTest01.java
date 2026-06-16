2cae5d36084dbf798e99ebf693b2fee88b5079cb
Nattan <natanaelsouzza123@gmail.com>
2026-06-16 09:09:10 -0300

    Update WrapperTest01.java

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -1,5 +1,25 @@
 package academy.devdojo.maratonajava.javacore.Pwrapper.test;
 
 public class WrapperTest01 {
-    
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        double doubleP = 1D;
+        float floatP = 1F;
+        char charP = 'A';
+        boolean booleanP = true;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        long longW = 10L;
+        Double doubleW = 1D;
+        Float floatW = 1F;
+        Character charW = 'A';
+        Boolean booleanW = true;
+
+        System.out.println(intW);
+    }
 }
