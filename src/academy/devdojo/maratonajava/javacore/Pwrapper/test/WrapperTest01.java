1c7aff8e01e1615c94bf7a8502f0004f312fd130
Nattan <natanaelsouzza123@gmail.com>
2026-06-16 09:41:08 -0300

    Update WrapperTest01.java

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -2,24 +2,38 @@ package academy.devdojo.maratonajava.javacore.Pwrapper.test;
 
 public class WrapperTest01 {
     public static void main(String[] args) {
-        byte byteP = 1;
-        short shortP = 1;
-        int intP = 1;
-        long longP = 10L;
-        double doubleP = 1D;
-        float floatP = 1F;
-        char charP = 'A';
-        boolean booleanP = true;
+        // byte byteP = 1;
+        // short shortP = 1;
+        // int intP = 1;
+        // long longP = 10L;
+        // double doubleP = 1D;
+        // float floatP = 1F;
+        // char charP = 'A';
+        // boolean booleanP = true;
 
-        Byte byteW = 1;
-        Short shortW = 1;
+        Byte byteW = 1; // Autoboxing
+        // Short shortW = 1;
         Integer intW = 1;
-        long longW = 10L;
-        Double doubleW = 1D;
-        Float floatW = 1F;
-        Character charW = 'A';
-        Boolean booleanW = true;
+        // long longW = 10L;
+        // Double doubleW = 1D;
+        // Float floatW = 1F;
+        // Character charW = 'A';
+        // Boolean booleanW = true;
 
         System.out.println(intW);
+
+        int i = byteW; // Unboxing
+        
+        // Integer intW2 = Integer.parseInt("1");
+        // boolean isTrue = Boolean.parseBoolean("TruE");
+
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+
     }
 }
