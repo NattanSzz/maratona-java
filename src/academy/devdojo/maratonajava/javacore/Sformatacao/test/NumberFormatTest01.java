218b147e1bc5582072ada307b82077e8c182c601
Nattan <natanaelsouzza123@gmail.com>
2026-06-26 09:43:37 -0300

    NumberFormat

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localePT = Locale.of("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALIAN;
+        NumberFormat[] nf = new NumberFormat[4];
+
+        nf[0] = NumberFormat.getInstance();
+        nf[1] = NumberFormat.getInstance(localeJP);
+        nf[2] = NumberFormat.getInstance(localePT);
+        nf[3] = NumberFormat.getInstance(localeIT);
+
+        double value = 10_000.2130;
+
+        for (NumberFormat n : nf) {
+            System.out.println(n.format(value));
+        }
+        
+    }
+}
