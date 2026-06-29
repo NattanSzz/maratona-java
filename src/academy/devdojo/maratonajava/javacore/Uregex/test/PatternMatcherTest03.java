eba93dee8a3a1add4bc06b6370c12115f24a5059
Nattan Souza <natanaelsouzza123@gmail.com>
2026-06-28 21:08:46 -0300

    Create PatternMatcherTest03.java

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d Tudo o que é digito
+        // \D Tudo o que não é digito
+        // \s Todos os espaços em branco
+        // \S Todos os caracteres que não são espaços em branco
+        // \w a-zA-Z, digitos, _
+        // \W Tudo que não for incluso no \w
+        // []
+        String regex = "0[xX][1-9A-Fa-f]";
+        String text2 = "12 0x 0X 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text2);
+        System.out.println("Texto: " + text2);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex: " + regex);
+        System.out.println("Posições encontradas");
+        while(matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+        int numberHex = 0x59F86A;
+        System.out.println(numberHex);
+    }
+}
