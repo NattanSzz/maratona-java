be4db56448467da48172ac4715d6a42b3d4cdef9
Nattan Souza <natanaelsouzza123@gmail.com>
2026-06-30 19:07:29 -0300

    Update SmartPhone.java

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/SmartPhone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/SmartPhone.java
@@ -9,6 +9,20 @@ public class SmartPhone {
         this.brand = brand;
     }
 
+    // reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null 
+    // simetrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // transitividade: para x, y, z diferentes de null, se x.equals(y) == true, e x.equals(z) == true, logo y.equals(z) == true
+    // consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) deve retornar false
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        SmartPhone smartPhone = (SmartPhone) obj;
+        return serialNumber != null && serialNumber.equals(smartPhone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
