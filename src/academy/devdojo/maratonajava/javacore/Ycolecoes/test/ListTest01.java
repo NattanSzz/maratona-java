a5ac031596ca90bc7fad13366e3b89b9f18267f2
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-01 08:39:00 -0300

    Update ListTest01.java

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
+        List<String> names2 = new ArrayList<>();
         names.add("Willian");
-        names.add("DevDojo Academy");
+        names.add("DevDojo");
+        names2.add("Suane");
+        names2.add("Academy");
+        // names.remove("Willian");
 
         for(Object name : names) {
             System.out.println(name);
@@ -18,5 +22,8 @@ public class ListTest01 {
         for(int i = 0; i < names.size(); i++){
             System.out.println(names.get(i));
         }
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
     }
 }
