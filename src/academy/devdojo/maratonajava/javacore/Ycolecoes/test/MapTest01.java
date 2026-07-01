7794585f72bf2b4df34f54936d10b63f703b4524
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-01 17:17:00 -0300

    Update MapTest01.java

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,5 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTest01 {
-    
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mouze", "mouse");
+        map.put("vc", "você");
+        map.putIfAbsent("vc", "você");
+        System.out.println(map);
+
+        map.forEach((key, value) -> System.out.println(key + " -> " + value));
+    }
 }
