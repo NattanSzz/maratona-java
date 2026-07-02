17d3fd4fae18fd018dc3afa9034f41d4027e14ed
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-02 09:50:21 -0300

    Create GenericsTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        List<String> lst = new ArrayList<>();
+        lst.add("Willian");
+        lst.add("Suane");
+    }
+}
