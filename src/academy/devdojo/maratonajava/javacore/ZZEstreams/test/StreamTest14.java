a6d5454390a995049f3e6e8de2ac2c1bfb5948f1
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-05 20:21:25 -0300

    Create StreamTest14.java

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest14.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Book;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Category;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.PROMOTION;
+
+public class StreamTest14 {
+    private static List<Book> books = new ArrayList<>(List.of(
+        new Book("Pequeno Princpe", 8.99, Category.FANTASY),
+        new Book("Dune", 3.99, Category.FANTASY),
+        new Book("I Have No Mouth", 5.99, Category.DRAMA),
+        new Book("A Metamorfose", 2.99, Category.FANTASY),
+        new Book("Harry Potter", 5.99, Category.FANTASY),
+        new Book("Coraline", 1.99, Category.FANTASY),
+        new Book("Coraline", 1.99, Category.FANTASY),
+        new Book("Peter Pan", 4.00, Category.ROMANCE)));
+
+    public static void main(String[] args) {
+        Map<Category, Long> collect = books.stream().collect(Collectors.groupingBy(Book::getCategory, Collectors.counting()));
+        System.out.println(collect);
+    }
+    
+}
