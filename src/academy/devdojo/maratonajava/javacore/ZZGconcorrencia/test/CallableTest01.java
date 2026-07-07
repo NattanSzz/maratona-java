8ff3fbeac6886fa1d431673bd73dbdca4dc7ea75
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-07 12:58:49 -0300

    Create CallableTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CallableTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CallableTest01 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        RandomNumberCallable randomNumberCallable = new RandomNumberCallable();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<String> future = executorService.submit(randomNumberCallable);
+        String s = future.get();
+        System.out.println("Programa finalizado");
+        executorService.shutdown();
+    }
+}
+
+class RandomNumberCallable implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        int count = ThreadLocalRandom.current().nextInt(1, 11);
+        for(int i = 0; i < count; i++){
+            System.out.printf("%s executando uma tarefa callable...%n", Thread.currentThread().getName());
+        }
+        return String.format("%s finished and the random number is %d%n", Thread.currentThread().getName(), count);
+    }
+    
+}
