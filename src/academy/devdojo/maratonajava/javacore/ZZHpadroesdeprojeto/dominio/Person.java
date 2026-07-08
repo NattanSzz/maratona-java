c43b2aeb41c414e8ca53c7810a0e91d955e9d2d2
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-08 11:46:15 -0300

    builder

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/Person.java
@@ -0,0 +1,63 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+
+    public Person(String firstName, String lastName, String username, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static class PersonBuilder {
+        private String firstName;
+        private String lastName;
+        private String username;
+        private String email;
+
+        public PersonBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public PersonBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public PersonBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(firstName, lastName, username, email);
+        }
+    }
+    
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/test/BuilderPatternTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        Person build = new Person.PersonBuilder()
+            .firstName("Willian")
+            .lastName("Suane")
+            .username("devdojo")
+            .email("")
+            .email("willian.suane@devdojo.academy")
+            .build();
+    }
+}
