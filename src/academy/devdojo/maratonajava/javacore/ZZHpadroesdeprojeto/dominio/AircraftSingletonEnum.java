2c878a8a8cf0103f14d1bb07ba307178962a29ec
Nattan Souza <natanaelsouzza123@gmail.com>
2026-07-09 09:09:56 -0300

    padrão enum

--- src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonEnum.java
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonEnum.java
@@ -1,5 +1,19 @@
 package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
 
-public class AircraftSingletonEnum {
-    
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AircraftSingletonEnum {
+    INSTANCE;
+    private final Set<String> availableSeats;
+
+    AircraftSingletonEnum() {
+        this.availableSeats = new HashSet<>();
+        this.availableSeats.add("1A");
+        this.availableSeats.add("1B");
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
 }
